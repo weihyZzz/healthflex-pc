807f7fa6caabcad8cb7a8b133446b3a170964dab
ustbweihy <56748102+weihyZzz@users.noreply.github.com>
2023-11-20 19:34:30 +0800

    feat: 修改接口调用的逻辑，适应新的接口规范

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -24,11 +24,11 @@ export default () => {
     const res = await login({
       variables: values,
     });
-    if (res.data.login) {
-      message.success("登录成功");
+    if (res.data.login.code === 200) {
+      message.success(res.data.login.message);
       return;
     }
-    message.error("登录失败");
+    message.error(res.data.login.message);
   };
   return (
     <div className={styles.container}>
@@ -74,7 +74,7 @@ export default () => {
               }
               return "获取验证码";
             }}
-            phoneName="phone"
+            phoneName="tel"
             name="code"
             rules={[
               {
@@ -89,10 +89,10 @@ export default () => {
                   tel,
                 },
               });
-              if (res.data.sendCodeMsg) {
-                message.success(`获取验证码成功！`);
+              if (res.data.sendCodeMsg.code === 200) {
+                message.success(res.data.sendCodeMsg.message);
               } else {
-                message.error("获取验证码失败!");
+                message.error(res.data.sendCodeMsg.message);
               }
             }}
           />

18479f83bacf4d4ef22ac376cbee7f2eea406c3f
ustbweihy <56748102+weihyZzz@users.noreply.github.com>
2023-11-20 11:01:54 +0800

    feat: 开发页面路由，配置login,home,page404页面

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ApolloProvider } from '@apollo/client'
-import { client } from './utils/apollo.ts'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { client } from "./utils/apollo.ts";
+import { ROUTE_CONFIG } from "./routes/index.ts";
+import Page404 from "./containers/Page404/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <ApolloProvider client={client}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {ROUTE_CONFIG.map((item) => (
+          <Route path={item.path} key={item.key} element={<item.element />} />
+        ))}
+        <Route path="*" element={<Page404/>} />
+      </Routes>
+    </BrowserRouter>
   </ApolloProvider>
-)
+);
